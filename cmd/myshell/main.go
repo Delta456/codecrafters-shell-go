19fccd007a40458bbcdc9bffbb105ff8d29b2d44
Swastik <swstkbaranwal@gmail.com>
2024-05-24 23:30:46 +0530

    pass 2nd stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		input, err := stdin.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		cmdNotFound(input)
+	}
+}
+
+func cmdNotFound(cmd string) {
+	// Trim spaces because the line endings are still CRLF.
+	cmd = strings.TrimSpace(cmd)
+	fmt.Printf("%s: command not found\n", cmd)
 }
